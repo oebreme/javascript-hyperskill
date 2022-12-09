374f5ca0e35364ecdcd254de6111964fef17fbed
[oebreme] <Lenk.Johannes@schueler.6072-fuerth.de>
2022-12-09 21:09:37 +0100

    Initial commit

---
+++ hyperskill/coffee-machine.js
@@ -0,0 +1,10 @@
+// Use "input()" to input a line from the user
+// Use "input(str)" to print some text before requesting input
+// You will need this in the following stages
+console.log("Starting to make a coffee\n" +
+    "Grinding coffee beans\n" +
+    "Boiling water\n" +
+    "Mixing boiled water with crushed coffee beans\n" +
+    "Pouring coffee into the cup\n" +
+    "Pouring some milk into the cup\n" +
+    "Coffee is ready!")
